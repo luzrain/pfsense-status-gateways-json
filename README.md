b990431d43916ad2fc26ccf53babe597ea909efa
anton.z <anton.z@live.com>
2021-06-30 00:10:06 +0300

    Codestyle compliance; Key moved to constant SECURITY_KEY; Added header Content-Type: application/json; Rounded inKbps and outKbps; Removed friendlyiface key names from result array; Removed lan interface (isn`t gateway); Remove useless $realif ref from get_interface_rates function; Remove useless $status == "none" condition; Readme update.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Module retrieves pfSense 2.x Dashboard Gateway Status and Network Traffic Graphs
 "status" can be "force_down", "down", "loss" (for packet-loss warning), "delay" (for latency
 warning), or "okay" for online.
 
-**WARNING**: This module bypasses the normal pfSense dashboard login, so it should be set with a different "key" of your choosing in the PHP code.  The default is shown below:
+**WARNING**: This module bypasses the normal pfSense dashboard login, so it should be set with a different "SECURITY_KEY" constant of your choosing in the PHP code. The default is shown below:
 
 `
 http://pfSenseIP/status_gateways_json.php?key=pfsense 
@@ -19,44 +19,39 @@ requested via an additional parameter as indicated below:
 http://pfSenseIP/status_gateways_json.php?key=pfsense&rates=1 
 `
 
-To install, simply change the "key" in the code below, then drop
+To install, simply change the "SECURITY_KEY" constant in the code below, then drop
 the status_gateways_json.php file into your pfSense "**/usr/local/www**"
 directory and you are set to go! 
 
 JSON output (&rates=1 version) should look something like this:
 
 ```
-{
-    "lan": {
-        "inKbps": 188.36579043272,
-        "outKbps": 2295.1290776905,
-        "interface": "em0"
-    },
-    "wan": {
-        "inKbps": 132.32223032712,
-        "outKbps": 307.77712136859,
+[
+    {
         "interface": "em1",
         "friendlyiface": "wan",
         "name": "GW_WAN",
-        "status": "okay",
+        "status": "online",
         "monitorip": "x.x.x.x",
         "sourceip": "x.x.x.x",
         "gatewayip": "x.x.x.x",
         "delay": "10.3ms",
-        "loss": "0%"
+        "loss": "0%",
+        "inKbps": 132.32,
+        "outKbps": 307.77
     },
-    "opt1": {
-        "inKbps": 1690.5506271424,
-        "outKbps": 197.42589503826,
+    {
         "interface": "em2",
         "friendlyiface": "opt1",
         "name": "GW_OPT1",
-        "status": "okay",
+        "status": "online",
         "monitorip": "x.x.x.x",
         "sourceip": "x.x.x.x",
         "gatewayip": "x.x.x.x",
         "delay": "25.5ms",
-        "loss": "0%"
+        "loss": "0%",
+        "inKbps": 1690.55,
+        "outKbps": 197.42
     }
-}
+]
 ```
--- status_gateways_json.php
+++ status_gateways_json.php
@@ -1,43 +1,43 @@
 <?php
 /*
-	/usr/local/www/status_gateways_json.php
-	by Alexander Morris
-	v0.13 20210325, added "gatewayip" for each interface
-	v0.12 20170312, fetch rates by using friendlyiface name and added more info on gateway names, interface, etc.
-	v0.11 20160421, only "clean" interface name if starts with "gw_"
-	v0.1 20150630, for pfSense 2.x
+    /usr/local/www/status_gateways_json.php
+    by Alexander Morris
+    v0.13 20210325, added 'gatewayip' for each interface
+    v0.12 20170312, fetch rates by using friendlyiface name and added more info on gateway names, interface, etc.
+    v0.11 20160421, only 'clean' interface name if starts with 'gw_'
+    v0.1 20150630, for pfSense 2.x
 
-	Module retrieves pfSense 2.x Dashboard Gateway Status and Traffic
-	Graphs data in JSON format.  "status" can be "force_down", 
-	"down", "loss" (for packet-loss warning), "delay" (for latency
-	warning), or "okay" for online.
+    Module retrieves pfSense 2.x Dashboard Gateway Status and Traffic
+    Graphs data in JSON format.  'status' can be 'force_down', 
+    'down', 'loss' (for packet-loss warning), 'delay' (for latency
+    warning), or 'online'.
 
-	WARNING: This module bypasses the normal pfSense dashboard 
-	login, so it should be set with a different "key" of your 
-	choosing in the PHP code.  The default is shown below:
+    WARNING: This module bypasses the normal pfSense dashboard 
+    login, so it should be set with a different 'key' of your 
+    choosing in the PHP code.  The default is shown below:
 
-	http://pfSenseIP/status_gateways_json.php?key=pfsense 
+    http://pfSenseIP/status_gateways_json.php?key=pfsense 
 
-	Since there is additional overhead to calculate data rates for 
-	each connection ("inKbps", "outKbps"), it can be optionally 
-	requested via an additional parameter as indicated below:
+    Since there is additional overhead to calculate data rates for 
+    each connection ('inKbps', 'outKbps'), it can be optionally 
+    requested via an additional parameter as indicated below:
 
-	http://pfSenseIP/status_gateways_json.php?key=pfsense&rates=1 
+    http://pfSenseIP/status_gateways_json.php?key=pfsense&rates=1 
 
-	To install, simply change the "key" in the code below, then drop 
-	the status_gateways_json.php file into your pfSense "/usr/local/www" 
-	directory and you are set to go! 
+    To install, simply change the 'SECURITY_KEY' constant in the code below, then drop 
+    the status_gateways_json.php file into your pfSense '/usr/local/www' 
+    directory and you are set to go! 
 
-	THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
-	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
-	AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-	AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
-	OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-	POSSIBILITY OF SUCH DAMAGE.
+    THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+    AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+    AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+    OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+    POSSIBILITY OF SUCH DAMAGE.
 */
 
 ##|+PRIV
@@ -47,56 +47,53 @@
 ##|*MATCH=status_gateways_json.php*
 ##|-PRIV
 
-##change key, used for quick and dirty security outside of GUI login
-if ($_GET['key'] != "pfsense") die("invalidRequest");
+//change key, used for quick and dirty security outside of GUI login
+define('SECURITY_KEY', 'pfsense');
 
-require("interfaces.inc");
+if ($_GET['key'] !== SECURITY_KEY) {
+    http_response_code(401);
+    echo '401 Unauthorized';
+    exit;
+}
+
+require('interfaces.inc');
 
 $a_gateways = return_gateways_array();
 $gateways_status = return_gateways_status(true);
+$pfgateways = [];
 
-$pfgateways = array();
-
-function get_interface_rates($iface, &$inKbps, &$outKbps, &$realif)
+function get_interface_rates($iface, &$inKbps, &$outKbps)
 {
-  $realif = get_real_interface($iface);
-  $ifinfo1 = pfSense_get_interface_stats($realif);
-  $tmrStart = microtime(true);
-  usleep(100000);
-  $ifinfo2 = pfSense_get_interface_stats($realif);
-  $totTime = microtime(true)-$tmrStart;
-  $inKbps = abs($ifinfo2['inbytes']-$ifinfo1['inbytes'])*(1/$totTime)/1000*8;
-  $outKbps = abs($ifinfo2['outbytes']-$ifinfo1['outbytes'])*(1/$totTime)/1000*8;
-}
-
-if ($_GET['rates'] == 1) {
-  get_interface_rates('lan',$inKbps,$outKbps,$realif);
-  $pfgateways["lan"]["inKbps"] = $inKbps;
-  $pfgateways["lan"]["outKbps"] = $outKbps;
-  $pfgateways["lan"]["interface"] = $realif;
+    $realif = get_real_interface($iface);
+    $ifinfo1 = pfSense_get_interface_stats($realif);
+    $tmrStart = microtime(true);
+    usleep(100000);
+    $ifinfo2 = pfSense_get_interface_stats($realif);
+    $totTime = microtime(true) - $tmrStart;
+    $inKbps = round(abs($ifinfo2['inbytes'] - $ifinfo1['inbytes']) * (1 / $totTime) / 1000 * 8, 2);
+    $outKbps = round(abs($ifinfo2['outbytes'] - $ifinfo1['outbytes']) * (1 / $totTime) / 1000 * 8, 2);
 }
 
 foreach ($gateways_status as $a_gateway) {
-  $iface = $a_gateways[$a_gateway['name']]['friendlyiface'];
-  if ($_GET['rates'] == 1) {
-    get_interface_rates($iface,$inKbps,$outKbps,$realif);
-    $pfgateways[$iface]["inKbps"] = $inKbps;
-    $pfgateways[$iface]["outKbps"] = $outKbps;
-  }
-  $realif = $a_gateways[$a_gateway['name']]['interface'];
-  $status = $a_gateway['status'];
-  if ($status == "none") $status = "okay";
-  $pfgateways[$iface]['interface'] = $realif;
-  $pfgateways[$iface]['friendlyiface'] = $iface;
-  $pfgateways[$iface]['name'] = $a_gateway['name'];
-  $pfgateways[$iface]['status'] = $status;
-  $pfgateways[$iface]['monitorip'] = $a_gateway['monitorip'];
-  $pfgateways[$iface]['sourceip'] = $a_gateway['srcip'];
-  $pfgateways[$iface]['gatewayip'] = $a_gateways[$a_gateway['name']]['gateway'];
-  $pfgateways[$iface]['delay'] = $a_gateway['delay'];
-  $pfgateways[$iface]['loss'] = $a_gateway['loss'];
+    $realif = $a_gateways[$a_gateway['name']]['interface'];
+    $iface = $a_gateways[$a_gateway['name']]['friendlyiface'];
+    $gateway = [];
+    $gateway['interface'] = $realif;
+    $gateway['friendlyiface'] = $iface;
+    $gateway['name'] = $a_gateway['name'];
+    $gateway['status'] = $a_gateway['status'];
+    $gateway['monitorip'] = $a_gateway['monitorip'];
+    $gateway['sourceip'] = $a_gateway['srcip'];
+    $gateway['gatewayip'] = $a_gateways[$a_gateway['name']]['gateway'];
+    $gateway['delay'] = $a_gateway['delay'];
+    $gateway['loss'] = $a_gateway['loss'];
+    if ($_GET['rates'] === '1') {
+        get_interface_rates($iface, $inKbps, $outKbps);
+        $gateway['inKbps'] = $inKbps;
+        $gateway['outKbps'] = $outKbps;
+    }
+    $pfgateways[] = $gateway;
 }
 
+header('Content-Type: application/json');
 echo json_encode($pfgateways);
-
-?>
